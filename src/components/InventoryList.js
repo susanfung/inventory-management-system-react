5b38adfd367714851192fbf86bdbb628aa968055
Susan Fung <fung_susan@hotmail.com>
2020-07-15 15:41:21 -0600

    Modified style of Inventory List

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,36 +1,50 @@
 import React from "react";
 import './InventoryList.css';
-import { Button } from 'antd';
+import { Button, Card, Collapse } from 'antd';
 import { DeleteOutlined } from '@ant-design/icons';
+
+const { Panel } = Collapse;
+
+const genExtra = () => (
+  <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
+);
 
 class InventoryList extends React.Component {
   render() {
     return (
       <div className="inventory-list item-list">
         {this.props.inventory.map(item => (
-          <div className="inventory-item" key={item.itemId}>
-            <div className="button">
-              <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
-            </div>
-    
-            <div className="inventory-info">
-              <div className="inventory-head">
-                <span className="inventory-name">{item.itemName}</span>
+          <Card
+            key={item.itemId}
+            style={{ marginBottom: 16 }}
+            type="inner"
+            title={
+              <div className="inventory-name">
+                {item.itemName}
               </div>
-
-              <div className="details">
-                <span className="label-item">Date Added: </span>
-                <span className="add-date">{item.addDate}</span>
-              </div>
-
-              <div className="details">
-                <span className="label-item">Location: </span>
-                <span>{item.location}</span>
-              </div>
-
+            }
+            extra={genExtra()}
+          >
+            <Collapse expandIconPosition="right" >
+              <Panel
+                header={
+                  <div className="inventory-info">
+                  <div className="details">
+                    <span className="label-item">Date Added: </span>
+                    <span className="add-date">{item.addDate}</span>
+                  </div>
+  
+                  <div className="details">
+                    <span className="label-item">Location: </span>
+                    <span>{item.location}</span>
+                  </div>  
+                </div>
+                }
+              >
               <div className="inventory-notes">{item.notes}</div>
-            </div>
-          </div>
+              </Panel>
+            </Collapse>
+          </Card>
         ))}
       </div>
     )
