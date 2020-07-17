187ab91b469e3f0b22f7bcfad8a4d5862d5a5d9b
Susan Fung <fung_susan@hotmail.com>
2020-07-17 10:08:29 -0600

    Changed Inventory List to table layout

--- src/components/InventoryList.js
+++ src/components/InventoryList.js
@@ -1,52 +1,33 @@
 import React from "react";
-import './InventoryList.css';
-import { Button, Card, Collapse } from 'antd';
-import { DeleteOutlined } from '@ant-design/icons';
+import { Table } from 'antd';
 
-const { Panel } = Collapse;
-
-const genExtra = () => (
-  <Button type="primary" shape="circle" icon={<DeleteOutlined />} />
-);
+const columns = [
+  {
+    title: 'Item',
+    dataIndex: 'itemName',
+    sorter: (a, b) => {return a.itemName.localeCompare(b.itemName)},
+  },
+  {
+    title: 'Location',
+    dataIndex: 'location',
+    sorter: (a, b) => {return a.location.localeCompare(b.location)},
+  },
+  {
+    title: 'Date Added',
+    dataIndex: 'addDate',
+    sorter: (a, b) => new Date(a.addDate) - new Date(b.addDate),
+  },
+];
 
 class InventoryList extends React.Component {
   render() {
     return (
-      <div className="inventory-list item-list">
-        {this.props.inventory.map(item => (
-          <Card
-            key={item.itemId}
-            style={{ marginBottom: 16 }}
-            type="inner"
-            title={
-              <div className="inventory-name">
-                {item.itemName}
-              </div>
-            }
-            extra={genExtra()}
-          >
-            <Collapse expandIconPosition="right" >
-              <Panel
-                header={
-                  <div className="inventory-info">
-                  <div className="details">
-                    <span className="label-item">Date Added: </span>
-                    <span className="add-date">{item.addDate}</span>
-                  </div>
-  
-                  <div className="details">
-                    <span className="label-item">Location: </span>
-                    <span>{item.location}</span>
-                  </div>  
-                </div>
-                }
-              >
-              <div className="inventory-notes">{item.notes}</div>
-              </Panel>
-            </Collapse>
-          </Card>
-        ))}
-      </div>
+      <Table
+        columns={columns}
+        dataSource={this.props.inventory}
+        onChange={this.onChange}
+        pagination={{ defaultPageSize: 10, showSizeChanger: true }}
+      />
     )
   }
 }
